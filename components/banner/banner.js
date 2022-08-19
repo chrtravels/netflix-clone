9207b00c44bb0b164849c777c626b4c0ceee1556
Chris Reynoso <chrtravels@gmail.com>
2022-08-19 16:48:25 -0700

    added banner component structure

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,7 +1,29 @@
 
-const Banner = () => {
+const Banner = (props) => {
+  // props being passed from index to Banner
+  const { title, subTitle, imgUrl } = props;
+
+  const handleOnPlay = () => {
+    console.log('handleOnPlay');
+  }
+
   return (
-    <div>Hi Banner!</div>
+    <div>
+      <h3>{title}</h3>
+      <h3>{subTitle}</h3>
+      <button onClick={handleOnPlay}>Play</button>
+      <div
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          backgroundSize: 'cover',
+          backgroundPosition: '50% 50%'
+        }}
+      ></div>
+    </div>
+
   )
 }
 
