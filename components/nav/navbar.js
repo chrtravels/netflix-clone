622c6e6ddd8b9febf12a7154616ae83a16e3357d
Chris Reynoso <chrtravels@gmail.com>
2022-09-02 18:14:31 +0900

    navbar styling:

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -3,25 +3,35 @@ import styles from './navbar.module.css'
 const NavBar = (props) => {
   const { username } = props;
   return (
-    <div>
-      NavBar
-      <p>{username}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
-      </ul>
-
-      <nav>
-        <div>
-          <button>
-            <p>{username}</p>
-          </button>
-
-          <div>
-            <a>Sign out</a>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>
+            Netflix
           </div>
-        </div>
-      </nav>
+        </a>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p styles={styles.username}>{username}</p>
+              {/* Expand more icon */}
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
