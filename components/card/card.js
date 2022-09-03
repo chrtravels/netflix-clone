c95b0d3058b9e04749f9730070e70d83a427d2d8
Chris Reynoso <chrtravels@gmail.com>
2022-09-04 08:21:05 +0900

    added error image handling & default card image

--- components/card/card.js
+++ components/card/card.js
@@ -1,14 +1,21 @@
 import styles from './card.module.css'
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Card = (props) => {
 
-  const { imgUrl, size } = props;
+  const { imgUrl = "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80", size = "medium" } = props;
 
+  const [imgSrc, setImgSrc] = useState(imgUrl);
   const classMap = {
     'large': styles.lgItem,
     'medium': styles.mdItem,
     'small': styles.smItem
+  }
+
+  const handleOnError = () => {
+    console.log('test');
+    setImgSrc("https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80");
   }
 
   return (
@@ -17,9 +24,10 @@ const Card = (props) => {
       Card
       <div className={classMap[size]}>
         <Image
-          src={imgUrl}
+          src={imgSrc}
           alt="image"
           layout='fill'
+          onError={handleOnError}
           className={styles.cardImg}
         />
       </div>
