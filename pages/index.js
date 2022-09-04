b8f2fd9dfb8664829a9abf5b1c99795607544718
Chris Reynoso <chrtravels@gmail.com>
2022-09-04 21:02:31 +0900

    section cards structure

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Banner from '../components/banner/banner';
 import NavBar from '../components/nav/navbar';
 import Card from '../components/card/card';
 
+import SectionCards from '../components/card/section-cards';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -20,6 +22,8 @@ export default function Home() {
       subTitle="a very cute dog"
       imgUrl="/static/clifford.webp"
       />
+
+      <SectionCards title="Disney" />
 
       <Card
         imgUrl='/static/clifford.webp'
