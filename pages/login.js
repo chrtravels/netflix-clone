49bfbe25fc45f53f1f4edcc1cbdede2daf65c953
Chris Reynoso <chrtravels@gmail.com>
2022-09-08 13:54:11 +0900

    added email validation

--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
@@ -5,11 +6,26 @@ import Image from "next/image";
 import styles from "../styles/login.module.css"
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [userMsg, setUserMsg] = useState('')
+
+  const handleOnChangeEmail = (e) => {
+    setUserMsg('');
+    const email = e.target.value;
+    setEmail(email);
+  }
 
   const handleLoginWithEmail = (e) => {
     console.log('login button')
     e.preventDefault();
-  }
+
+    if (email) {
+      // route to dashboard
+
+    } else {
+      setUserMsg('Enter a valid email address');
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -40,8 +56,9 @@ const Login = () => {
             type="text"
             placeholder="Email address"
             className={styles.emailInput}
+            onChange={handleOnChangeEmail}
           />
-          <p className={styles.userMsg}></p>
+          <p className={styles.userMsg}>{userMsg}</p>
           <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
             Sign In
           </button>
