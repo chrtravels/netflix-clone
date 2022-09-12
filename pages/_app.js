b43aaa61e0ffe8e2018de8f8bb48320b6309ef23
Chris Reynoso <chrtravels@gmail.com>
2022-09-12 15:33:23 +0900

    add routing based on login status

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import { magic } from '../lib/magic-client';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn()
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    }; checkLogin();
+  }, [])
+
   return <Component {...pageProps} />
 }
 
