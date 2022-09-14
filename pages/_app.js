a0cad142138594c5cd3d9a05516b30ced994ae4b
Chris Reynoso <chrtravels@gmail.com>
2022-09-14 14:43:40 +0900

    added modal to {videoId}

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,17 @@ import Loading from '../components/loading/loading';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // const checkLogin = async () => {
-    //   const isLoggedIn = await magic.user.isLoggedIn()
-    //   if (isLoggedIn) {
-    //     router.push('/');
-    //   } else {
-    //     router.push('/login');
-    //   }
-    // }; checkLogin();
+    const checkLogin = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn()
+      if (isLoggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    }; checkLogin();
   }, [])
 
   useEffect(() => {
--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,10 +1,27 @@
 import { useRouter } from "next/router";
+import Modal from 'react-modal';
+
+import styles from '../../styles/video.module.css';
+
+Modal.setAppElement('#__next');
+
 
 const Video = () => {
   const router = useRouter()
   console.log(router);
 
-  return <div>Video Page {router.query.videoId}</div>
-}
+  return (
+    <div>Video Page {router.query.videoId}
+        <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        overlayClassName={styles.overlay}
+        >
+        <div>Modal body</div>
+        </Modal>
+    </div>
+  );
+};
 
 export default Video;
