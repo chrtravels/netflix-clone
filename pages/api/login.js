88629efac39d7c397ba0b7c82b98954b17ee3d10
Chris Reynoso <chrtravels@gmail.com>
2022-09-22 15:47:08 +0900

    create token using jsonwebtoken

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,32 @@
 import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
+import { queryHasuraGQL } from "../../lib/db/hasura";
 
 export default async function login (req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
       const didToken = auth.substring(7);
-
+      // Invote magic
       const metadata = await magicAdmin.users.getMetadataByToken(didToken)
       console.log({ metadata });
+      console.log({ queryHasuraGQL })
+
+      // Create JWT
+      const token = jwt.sign(
+        {
+          ...metadata,
+          "iat": Math.floor(Date.now() / 1000),
+          "exp": Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+          },
+      },
+      "thisisasecretthisisasupersecret2367",
+      );
+      console.log({ token });
 
       res.send({ done: true });
     } catch(error) {
