0d7abd6068c0ea1b8213845c8865037bbd3e0f6b
Chris Reynoso <chrtravels@gmail.com>
2022-09-28 11:52:53 +0900

    added like dislike btns & styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -8,6 +8,8 @@ import clsx from "classnames";
 import NavBar from '../../components/nav/navbar';
 
 import { getYoutubeVideoById } from "../../lib/videos";
+import Like from '../../components/icons/like-icon';
+import DisLike from '../../components/icons/dislike-icon';
 
 Modal.setAppElement('#__next');
 
@@ -59,6 +61,21 @@ const Video = ({ video }) => {
         src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
         frameborder="0"
         ></iframe>
+
+        <div className={styles.likeDislikeBtnWrapper}>
+          <div className={styles.likeBtnWrapper}>
+            <button>
+              <div className={styles.btnWrapper}>
+                <Like />
+              </div>
+            </button>
+          </div>
+          <button>
+            <div className={styles.btnWrapper}>
+              <DisLike />
+            </div>
+          </button>
+        </div>
 
         <div className={styles.modalBody}>
           <div className={styles.modalBodyContent}>
