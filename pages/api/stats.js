3d352c1d41448e10c95596da3c56dab160b5cd1c
Chris Reynoso <chrtravels@gmail.com>
2022-09-29 13:29:19 +0900

    added jwt token verification

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,6 +1,21 @@
+import jwt from 'jsonwebtoken';
+
 export default async function stats(req, res) {
   if (req.method === "POST") {
-    console.log({cookies: req.cookies});
-    res.send({msg: "it works"});
+
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        res.status(403).send({});
+      } else {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log({ decoded });
+        res.send({ msg: "it works", decoded });
+      }
+    } catch(error) {
+      console.error("Error occurred /stats", error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
+
   }
 }
