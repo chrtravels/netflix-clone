d0be4da8228d478e0494313ee8433166080f1d11
Chris Reynoso <chrtravels@gmail.com>
2022-09-29 15:10:23 +0900

    added findVideoIdByUser for stats API

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,16 +1,21 @@
 import jwt from 'jsonwebtoken';
+import { findVideoIdByUser } from '../../lib/db/hasura';
 
 export default async function stats(req, res) {
   if (req.method === "POST") {
 
     try {
       const token = req.cookies.token;
+
       if (!token) {
         res.status(403).send({});
       } else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log({ decoded });
-        res.send({ msg: "it works", decoded });
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+        const videoId = req.query.videoId;
+        const findVideoId = await findVideoIdByUser(token,  userId, videoId);
+        res.send({ msg: "it works", decodedToken, findVideoId });
       }
     } catch(error) {
       console.error("Error occurred /stats", error);
