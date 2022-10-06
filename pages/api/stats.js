0489eefe4d67fffc351b2e0d61c08e8e7b6d7410
Chris Reynoso <chrtravels@gmail.com>
2022-10-06 13:34:01 +0900

    updated stats API to take dynamic values

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -10,21 +10,24 @@ export default async function stats(req, res) {
       if (!token) {
         res.status(403).send({});
       } else {
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const { videoId, favorited, watched = true } = req.body
 
-        const userId = decodedToken.issuer;
-        const videoId = req.query.videoId;
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-        const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
-        console.log(doesStatsExist.data.stats)
-        if (doesStatsExist.data.stats.length !== 0) {
-          // update it
-          const response = await updateStats(token, { userId, videoId: "CaimKeDcudo", favorited: 0, watched: true })
-          res.send({ msg: "it works", updateStats: response });
-        } else {
-          // add it
-          const response = await insertStats(token, { userId, videoId, favorited: 0, watched: false })
-          res.send({ msg: "it works", insertStats: response });
+          const userId = decodedToken.issuer;
+
+          const doesStatsExist = await findVideoIdByUser(token, userId, videoId);
+
+          if (doesStatsExist.data.stats.length !== 0) {
+            // update it
+            const response = await updateStats(token, { userId, videoId, favorited, watched })
+            res.send({ data: response });
+          } else {
+            // add it
+            const response = await insertStats(token, { userId, videoId, favorited, watched })
+            res.send({ data: response });
+          }
         }
       }
     } catch(error) {
