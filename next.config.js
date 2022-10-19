6a02c54ab804f4edb517548198c2afc33845a6d0
Chris Reynoso <chrtravels@gmail.com>
2022-10-19 12:23:45 +0900

    updated next config to ignore eslint

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'i.ytimg.com', 'yt3.ggpht.com'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
